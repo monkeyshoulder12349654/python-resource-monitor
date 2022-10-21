8b88f7ad2f9cf264e38c7733a5ff8deb996d23d9
Ric <marianir915@gmail.com>
2022-10-21 19:49:37 -0400

    wow

--- test.py
+++ test.py
@@ -18,6 +18,7 @@
 #sys.stdout.write("\r{0}".format(psutil.virtual_memory()[3]/1000000000))
  #       sys.stdout.write("\r\n")
   #      sys.stdout.flush()
+from colorama import Fore, Back, Style
 from tqdm import tqdm
 from time import sleep
 import sys
@@ -27,10 +28,13 @@ import psutil
 #print("MPBS DOWNLOAD: ")
 #print(psutil.net_io_counters().bytes_recv/1000000)
 
-#print(psutil.disk_usage('/').percent)
 
-with tqdm(total=100, desc='cpu     %', position=2) as cpubar, tqdm(total=10, desc='ram used%', position=0) as rambar, tqdm(total=10, desc='ram avlb%', position=1) as rambar2,tqdm(total=100, desc='MBS UP  %', position=3) as net1bar,tqdm(total=1000, desc='MBS DOWN%', position=4) as net2bar,tqdm(total=100, desc='Disk Use%', position=5) as diskbar1:
+#print(Fore.CYAN)
+#print(Back.BLACK)
+#print(Style.BRIGHT)
+with tqdm(colour = "red", total=100, desc='cpu     %', position=2) as cpubar, tqdm(total=10, desc='ram used%', position=0) as rambar, tqdm(total=10, desc='ram avlb%', position=1) as rambar2,tqdm(total=100, desc='MBS UP  %', position=3) as net1bar,tqdm(total=1000, desc='MBS DOWN%', position=4) as net2bar,tqdm(total=100, desc='Disk Use%', position=5) as diskbar1:
     while True:
+        
         
         rambar2.n=round(10*(psutil.virtual_memory().available/psutil.virtual_memory().total),2)
         rambar.n=round((psutil.virtual_memory().percent/10),2)
@@ -39,15 +43,14 @@ with tqdm(total=100, desc='cpu     %', position=2) as cpubar, tqdm(total=10, des
         net2bar.n=psutil.net_io_counters().bytes_recv/1000000
         diskbar1.n=psutil.disk_usage('/').percent
         
+        
         rambar.refresh()
         rambar2.refresh()
         cpubar.refresh()
         net1bar.refresh()
         net2bar.refresh()
         diskbar1.refresh()
-        #print('memory used:', , 'GB')
-        #print('memory available:', psutil.virtual_memory()[1]/1000000000, 'GB')
-
+        Style.RESET_ALL
         sleep(0.5)
         
 
