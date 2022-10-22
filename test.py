9d6727e07cec31164e1b0696f2b5cbb223cc3a3c
Malcolm Kahora <mango22mjk@gmail.com>
2022-10-21 20:20:24 -0400

    idk

--- test.py
+++ test.py
@@ -1,23 +1,40 @@
+class colors:
+  reset = '\033[0m'
+  bold = '\033[01m'
+  disable = '\033[02m'
+  underline = '\033[04m'
+  reverse = '\033[07m'
+  strikethrough = '\033[09m'
+  invisible = '\033[08m'
+  
+  class fg:
+      black = '\033[30m'
+      red = '\033[31m'
+      green = '\033[32m'
+      orange = '\033[33m'
+      blue = '\033[34m'
+      purple = '\033[35m'
+      cyan = '\033[36m'
+      lightgrey = '\033[37m'
+      darkgrey = '\033[90m'
+      lightred = '\033[91m'
+      lightgreen = '\033[92m'
+      yellow = '\033[93m'
+      lightblue = '\033[94m'
+      pink = '\033[95m'
+      lightcyan = '\033[96m'
+  class bg:
 
-#import psutil
-#psutil.cpu_percent()
-#psutil.virtual_memory()
-#dict(psutil.virtual_memory()._asdict())
-#psutil.virtual_memory().percent
-#79.2
-#psutil.virtual_memory().available * 100 / psutil.virtual_memory().total
-#20.8
-
-#from __future__ import print_function
-#import psutil
-#print(psutil.cpu_percent())
-#print(psutil.virtual_memory())  # physical memory usage
-#print('memory % used:', psutil.virtual_memory()[2])
+     black = '\033[40m'
+     red = '\033[41m'
+     green = '\033[42m'
+     orange = '\033[43m'
+     blue = '\033[44m'
+     purple = '\033[45m'
+     cyan = '\033[46m'
+     lightgrey = '\033[47m'
 
 
-#sys.stdout.write("\r{0}".format(psutil.virtual_memory()[3]/1000000000))
- #       sys.stdout.write("\r\n")
-  #      sys.stdout.flush()
 from tqdm import tqdm
 from time import sleep
 import sys
@@ -28,25 +45,18 @@ import psutil
 #print(psutil.net_io_counters().bytes_recv/1000000)
 
 #print(psutil.disk_usage('/').percent)
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
-print(f"{bcolors.WARNING}Warning: No active frommets remain. Continue?{bcolors.ENDC}")
+cpu = f"{colors.fg.purple}cpu     %{colors.reset}"
 
-with tqdm(total=100, desc='cpu     %', position=2, ascii='░▒█') as cpubar, \
-          tqdm(total=10, desc='ram used%', position=0,ascii=' #') as rambar, \
-          tqdm(total=10, desc='ram avlb%', position=1,ascii='░▒█') as rambar2, \
-          tqdm(total=100, desc=f'{bcolors.OKGREEN}MBS UP  %{bcolors.ENDC}', position=3,ascii='░▒█') as net1bar, \
-          tqdm(total=1000, desc='MBS DOWN%', position=4,ascii='░▒█') as net2bar, \
-          tqdm(total=100, desc='Disk Use%', position=5,ascii='░▒█') as diskbar1:
+
+print(f"{colors.bg.orange} {colors.fg.black} Warning: No active frommets remain. Continue?{colors.reset}")
+
+with tqdm(total=100, desc=cpu, position=2, ascii=' |') as cpubar, \
+          tqdm(colour="blue", total=10, desc='ram used%', position=0,ascii=' |') as rambar, \
+          tqdm(total=10, desc='ram avlb%', position=1,ascii=' |') as rambar2, \
+          tqdm(total=1000, desc=f'{colors.fg.green}MBS UP  %{colors.reset}', position=3,ascii=' |') as net1bar, \
+          tqdm(total=10000, desc='MBS DOWN%', position=4,ascii=' |') as net2bar, \
+          tqdm(total=100, desc='Disk Use%', position=5,ascii=' |') as diskbar1:
 
     while True:
         
